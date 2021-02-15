057307ded83adc9306f44212f90415ca45c90b2a
Ramy Nasser <ramy.assr@vodafone.com>
2021-02-15 14:06:56 +0200

    Initial Commit

---
+++ JenkiusCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkiusCI
+//
+//  Created by Ramy Nasser on 2/15/21.
+//  Copyright © 2021 Ramy Nasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
